c66af3f6a0f2fcbe1b8aef01ee9222c32de2b235
developershahiduzzamann <sabbir.mirpur58@gmail.com>
2025-12-06 12:28:19 +0600

    addad the transaction section

--- assets/js/toggle.js
+++ assets/js/toggle.js
@@ -1,12 +1,23 @@
-document.getElementById('cash-out').style.display='none'
+document.getElementById('cash-out').style.display='none';
+document.getElementById('transaction-money').style.display = 'none';
+
 
 document.getElementById('money-btn').addEventListener('click',function(){
     getToggle("add-money","block");
     getToggle("cash-out","none");
+    getToggle("transaction-money","none");
+    
     
 });
 
 document.getElementById('cash-out-btn').addEventListener('click',function(){
     getToggle("add-money","none");
     getToggle("cash-out","block");
+    getToggle("transaction-money","none");
 });
+
+document.getElementById('transaction-btn').addEventListener('click',function(){
+    getToggle("add-money","none");
+    getToggle("cash-out","none");
+    getToggle("transaction-money","block");
+})
